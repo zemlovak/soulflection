0d995b78e5c4788d6b0a303bdc1c2b588cc18288
Kristyna Zemlova <kr.zemlova@gmail.com>
2025-01-05 19:27:41 +0100

    FirebaseConfig updated

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,15 @@
-// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9ZbJW-dB8qTvyE5AsQkQIpYXOZGBPsE0" /* import.meta.env.FIREBASE_API_KEY */,
-  authDomain: "soulflection-95fc9.firebaseapp.com",
-  projectId: "soulflection-95fc9",
-  storageBucket: "soulflection-95fc9.appspot.com",
-  messagingSenderId: "658987669987",
-  appId: "1:658987669987:web:28112eedb9a9b45b7468d6",
-  measurementId: "G-DM1ZHREL23",
+  apiKey:  import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
