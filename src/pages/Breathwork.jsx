298aad02de37b0f2cf8c9823558f9ded24ca5366
Kristyna Zemlova <kr.zemlova@gmail.com>
2025-01-24 23:42:08 +0100

    New pages added

---
+++ src/pages/Breathwork.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+
+export const BreathworkPage = () => {
+    return (
+        <div className="min-w-96 px-8 py-8 mb-8 sm:px-12 bg-cyan-dark bg-opacity-25 rounded-xl">
+        <h2 className="text-white">Here goes the breathwork page content</h2>
+        <div className="w-full h-auto bg-cyan-dark rounded-lg px-6 py-4"></div>
+      </div>
+    );
+}
+ 
+export default BreathworkPage;
---
+++ src/pages/Yoga.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+
+export const YogaPage = () => {
+    return (
+        <div className="min-w-96 px-8 py-8 mb-8 sm:px-12 bg-cyan-dark bg-opacity-25 rounded-xl">
+        <h2 className="text-white">Here goes the yoga page content</h2>
+        <div className="w-full h-auto bg-cyan-dark rounded-lg px-6 py-4"></div>
+      </div>
+    );
+}
+ 
+export default YogaPage;
